b22652f0bdbf22dd36cb66d0a0730629ea348564
eanceleur <eric.lanceleur@gmail.com>
2018-08-26 23:44:25 +0200

    test with a 2 rows, 2 cols matrix

--- leds2pcf.py
+++ leds2pcf.py
@@ -7,8 +7,10 @@ class Leds :
   # a row = a led
   mapping_rows = ( 0x4000, 0x8000 )
   # R G B
-  mapping_colors = ( 0x0001, 0x0002, 0x0004 )
-
+  mapping_colors = ( 0x0100, 0x0200, 0x0400 )
+  # column
+  mapping_cols = ( 0x0001, 0x0002 )
+  
   # init smbus
   def __init__(self, i2cbus, pcf_address ):
     self.i2cbus = i2cbus
@@ -21,12 +23,12 @@ class Leds :
     p10p17 = ports >> 8
 	# depending on hardware, we need to inverse data as leds swith on on zero level
     p0p7 = ~ p0p7
-    p10p17 = ~ p10p17
+    p10p17 = p10p17 ^ 0xF0    # colors are not inversed
     self.bus.write_byte_data(self.pcf_address, p0p7 , p10p17 )
     print( "  i2c write data : 0x{0:X} 0x{1:X} 0x{2:X}".format( self.pcf_address, p0p7 & 0xFF, p10p17 & 0xFF ))
 
-  def turn_on( self, row, color ):
-    # switch on leds
+  def turn_on( self, row, col, color ):
+    
     ports = self.mapping_rows[ row ]
 
     if color == 'R':
@@ -38,6 +40,8 @@ class Leds :
     else:
       None
 
+    ports |= self.mapping_cols[col]
+	
     print( "ports = {0}".format( hex(ports)))
     
     self.write2pcf( ports )
--- main.py
+++ main.py
@@ -7,11 +7,35 @@ import leds2pcf
 # create object with i2cbus number and pcf adress
 leds = leds2pcf.Leds( 1, 0x20 )
 
+def the_loop():
+  while True:
+    for row in range(2):
+      for col in range(2):
+        for color in ['R','G','B']:
+          leds.turn_on( row, col, color )
+          time.sleep(2)
 
-for row in range(2):
-	for color in ['R','G','B']:
-		leds.turn_on( row, color )
-		time.sleep(2)
+def matrix():
+  matrix = [
+    [ 'R', 'B' ],
+    [ 'B', 'G' ],
+  ]
+  while True:
+    for row in range(len(matrix)):
+      for col in range(len(matrix[row])):
+        leds.turn_on( row, col, matrix[row][col] )
+
+
+print('Choose : \n')
+print(' 1 - loop rows, columns, colors\n')
+print(' 2 - matrix\n')
+
+f = input()
+f = int(f)
+if f == 1:
+  the_loop()
+elif f == 2:
+  matrix()
 
 leds.turn_off()
 
